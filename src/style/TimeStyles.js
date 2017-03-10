3c9d31a833dd32d8eb5d34f6aa395f2ca21540c5
“kwijikgit config --global user.email “joebalalaika@ya.ru <joebalalaika@ya.ru>
2017-03-10 21:42:43 +0100

    place time center

---
+++ src/style/TimeStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'aphrodite';
+
+
+export default StyleSheet.create({
+    time : {
+        position: 'absolute',
+        width: '15%',
+        height: '14%',
+        top: '3%',
+        left: '42%',
+        
+    }
+});
